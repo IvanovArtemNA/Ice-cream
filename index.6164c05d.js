51bc7a94139a35ab1a4ca302e71e4c91dc9dd937
IvanovArtemNA <113294828+IvanovArtemNA@users.noreply.github.com>
2022-10-12 18:54:37 +0000

    Deploying to gh-pages from @ IvanovArtemNA/ice-cream-team-project-1@cb3e5ac856406b29c9d612f8def882a9394ed88a 🚀

---
+++ index.6164c05d.js
@@ -0,0 +1,2 @@
+(()=>{function e(e){e.classList.toggle("is-hidden"),e.classList.toggle("opened")}document.addEventListener("click",(t=>{let n=null;const s=t.target.dataset.btn;"close"===s?(n=document.getElementsByClassName("opened")[0],e(n)):s&&(n=document.getElementById(s),e(n))}))})();
+//# sourceMappingURL=index.6164c05d.js.map
