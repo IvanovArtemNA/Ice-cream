dea982fdf4bd0846d2037b164761924424109cc4
IvanovArtemNA <113294828+IvanovArtemNA@users.noreply.github.com>
2022-10-13 00:38:34 +0000

    Deploying to gh-pages from @ IvanovArtemNA/ice-cream-team-project-1@861db9f2fc90a3c12d0123f2ebcd42a53f2515ee 🚀

--- index.6164c05d.js
+++
@@ -1,2 +0,0 @@
-(()=>{function e(e){e.classList.toggle("is-hidden"),e.classList.toggle("opened")}document.addEventListener("click",(t=>{let n=null;const s=t.target.dataset.btn;"close"===s?(n=document.getElementsByClassName("opened")[0],e(n)):s&&(n=document.getElementById(s),e(n))}))})();
-//# sourceMappingURL=index.6164c05d.js.map
--- index.8f75b864.js
+++
@@ -1,2 +0,0 @@
-(()=>{const e=document.querySelector("[data-mobile__menu-open]"),t=document.querySelector("[data-mobile__menu]");e.addEventListener("click",(()=>{const a="true"===e.getAttribute("aria-expanded")||!1;e.classList.toggle("is-open"),e.setAttribute("aria-expanded",!a),t.classList.toggle("is-open")}))})();
-//# sourceMappingURL=index.8f75b864.js.map
